d4d2dc48565c10acbe15c456ba81d6a712f741a1
Afxsoft <afxsoft@gmail.com>
2014-11-05 23:11:44 +0100

    update

---
+++ Application/Views/Layouts/login.tpl.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $viewTitle . ' | ' . $viewSiteName ?></title>
+    <base href="<?php echo HOST_ROOT; ?>" />
+    <!-- Styles -->
+    <!-- Bootstrap CSS -->
+    <link href="dashboard/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font awesome CSS -->
+    <link href="dashboard/css/font-awesome.min.css" rel="stylesheet">	
+    <!-- Custom Color CSS -->
+    <link href="dashboard/css/less-style.css" rel="stylesheet">	
+    <!-- Custom CSS -->
+    <link href="dashboard/css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="outer-page">
+      <?php echo $viewContent; ?>
+    </div>
+    <!-- Javascript files -->
+    <!-- jQuery -->
+    <script src="dashboard/js/jquery-1.11.1.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="dashboard/js/bootstrap.min.js"></script>
+    <!-- Respond JS for IE8 -->
+    <script src="dashboard/js/respond.min.js"></script>
+    <!-- HTML5 Support for IE -->
+    <script src="dashboard/js/html5shiv.js"></script>
+    <!-- Custom JS -->
+    <script src="dashboard/js/custom.js"></script>
+
+    <!-- Javascript for this page -->
+
+    <!-- Javascript for this page -->
+
+    <script type="text/javascript">
+
+    </script>
+  </body>
+</html>
