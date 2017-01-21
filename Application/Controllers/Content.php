3809e88258cb9d9952265087a32c002da1fb243d
afxsoft <aouloube@student.42.fr>
2017-01-22 00:38:13 +0100

    psr-3

--- Application/Controllers/Content.php
+++ Application/Controllers/Content.php
@@ -3,26 +3,32 @@
 namespace Application\Controllers;
 
 use Application\Controllers\AppController as MainController;
+use \Administration\Models\Article;
 
-class Content extends MainController {
+class Content extends MainController
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function indexAction() {
-       header('Location: /404');
-    } 
-    public function pageAction() {
+    public function indexAction()
+    {
+        header('Location: /404');
+    }
+
+    public function pageAction()
+    {
         global $connexion;
         $cx = $connexion->get_cx();
-        $modelArticle = new \Administration\Models\Article($cx);
+        $modelArticle = new Article($cx);
         $content = $modelArticle->getArticleByAlias($_GET['params']);
         if (!empty($content)) {
-            $calendar = FALSE;
-            if($_GET['params'] == "calendrier"){
-            $calendar = TRUE;
-                
+            $calendar = false;
+            if ($_GET['params'] == "calendrier") {
+                $calendar = true;
+
             }
             $this->add_data_view(array(
                 "viewTitle" => $content[0]->title,
@@ -30,7 +36,7 @@ class Content extends MainController {
                 "calendar" => $calendar,
                 "pageContent" => $content[0]
             ));
-        }else{
+        } else {
             header('Location: /404');
         }
     }
--- Application/Controllers/Login.php
+++ Application/Controllers/Login.php
@@ -4,22 +4,26 @@ namespace Application\Controllers;
 
 use Application\Controllers\AppController as MainController;
 use Library\Tools as Tools;
+use \Application\Models\User;
 
-class Login extends MainController {
+class Login extends MainController
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function indexAction() {
+    public function indexAction()
+    {
         global $connexion;
         $cx = $connexion->get_cx();
-        if(!empty($_SESSION['User'])){
-          header("location:/admin");
+        if (!empty($_SESSION['User'])) {
+            header("location:/admin");
         }
         if (!empty($_POST['login']) && !empty($_POST['password'])) {
-            $modelUser = new \Application\Models\User($cx);
-            $result = $modelUser->getUserLogin($_POST['login'],md5($_POST['password']));
+            $modelUser = new User($cx);
+            $result = $modelUser->getUserLogin($_POST['login'], md5($_POST['password']));
             if (!empty($result)) {
                 $_SESSION['User']['id'] = $result[0]->id;
                 $_SESSION['User']['name'] = $result[0]->nickname;
@@ -30,7 +34,7 @@ class Login extends MainController {
             }
         }
 
-        $this->add_data_view(array("viewTitle" => "Login","alert" => (!empty($alert)) ? $alert : ''));
+        $this->add_data_view(array("viewTitle" => "Login", "alert" => (!empty($alert)) ? $alert : ''));
         $this->set_layout('login');
     }
 
